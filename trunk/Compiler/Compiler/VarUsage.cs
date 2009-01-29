770aa2a250e4e23a965edcbb596201e54ea886b0
mikestedman@gmail.com <mikestedman@gmail.com@3eb2c810-ed5a-11dd-9059-71fac19e68c6>
2009-01-29 18:21:35 +0000

    Move VarUsage enum into its own source file.

---
+++ trunk/Compiler/Compiler/VarUsage.cs
@@ -0,0 +1,16 @@
+﻿namespace Compiler
+{
+   enum VarUsage
+   {
+      Unknown,
+      Hide,
+      Assigned,
+      Subroutine,
+      AssignedNumeric,
+      AssignedText,
+      AssignedDynArray,
+      Numeric,
+      Text,
+      DynArray
+   }
+}
